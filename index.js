ae527607ae035790d3433f398f7cba2f5e4430b1
Cschelb <85119920+Cschelb@users.noreply.github.com>
2022-12-09 23:12:37 -0500

    testing specific

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING_APTS || 'mongodb+srv:
 mongoose.connect(CONNECTION_STRING,mongooseOptions);
 app.use(cors({
     credentials: true,
-    origin: ["https://voluble-tanuki-375d22.netlify.app/", "http://localhost:3000"]
+    origin: "https://voluble-tanuki-375d22.netlify.app/"
 }));
 // app.use(cors({
 //                  credentials: true,
