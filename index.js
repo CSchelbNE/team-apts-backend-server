1abe6c7b7200902673b6dbe2aee348f8ba9c75d0
Cschelb <85119920+Cschelb@users.noreply.github.com>
2022-12-10 11:42:59 -0500

    pushing new db string

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const mongooseOptions = {
 }
 // DB_CONNECTION_STRING_APTS = mongodb+srv://APTS-final-project:supersecretpassword@cluster0.80l7cee.mongodb.net/APTS?retryWrites=true&w=majority
 // put above in environment variables (see A9), then erase these comments, thanks!
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING_APTS || 'mongodb+srv://APTS-final-project:supersecretpassword@cluster0.80l7cee.mongodb.net/APTS?retryWrites=true&w=majority';
+const CONNECTION_STRING = 'mongodb+srv://APTS-final-project:supersecretpassword@cluster0.80l7cee.mongodb.net/APTS?retryWrites=true&w=majority';
 mongoose.connect(CONNECTION_STRING,mongooseOptions);
 // DONT ADD THE EXTRA / TO THE END OF THE CORS ORIGINS
 app.use(cors({
